58a85f76a5e33334c1076588cc0e9e2e5cae6e74
Abdulazeez Raja <83156289+mc-jenia@users.noreply.github.com>
2023-03-27 05:09:41 +0300

    Update api.dart

--- lib/constant/api.dart
+++ lib/constant/api.dart
@@ -1,5 +1,5 @@
 class MyApi {
-  static const apiKey = 'e3d304e939340f69f639d7bb3c709475';
+  static const apiKey = '';
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String nowPlaying = '$baseUrl/movie/now_playing?api_key=$apiKey';
   static String imageUrl(String path) => 'https://image.tmdb.org/t/p/w500$path';
